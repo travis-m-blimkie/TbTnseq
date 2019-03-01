6f879d0bb9c822d90de622d2ff7d61da85ca1326
Travis <travis.m.blimkie@gmail.com>
2019-03-01 14:17:41 -0800

    Added generic renaming function to replace tool-specific ones.

---
+++ R/tbt_rename_columns.R
@@ -0,0 +1,13 @@
+
+# TbTnseq column renaming function ----------------------------------------
+
+
+tbt_rename_columns <- function(input_list, cond_name) {
+
+  for (i in 1:length(input_list)) {
+    colnames(input_list[[i]])[2] <- paste0(cond_name, "_", names(input_list)[i], "_", colnames(input_list[[i]])[2])
+  }
+
+  return(input_list)
+
+}
