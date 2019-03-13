d45612383af9ce8ee6922db1ab7b5c66bc7604c2
Travis <travis.m.blimkie@gmail.com>
2019-03-13 15:48:59 -0700

    Updated package version of tbt_rename column

--- R/tbt_rename.R
+++ R/tbt_rename.R
@@ -1,9 +1,7 @@
 
 # TbTnseq column renaming function ----------------------------------------
 
-#' Function which renames columns to be unique for that condition and replicate
-
-tbt_rename_columns <- function(input_list, cond_name) {
+tbt_rename <- function(input_list, cond_name) {
 
   for (i in 1:length(input_list)) {
     colnames(input_list[[i]])[2] <- paste0(cond_name, "_", names(input_list)[i], "_", colnames(input_list[[i]])[2])
